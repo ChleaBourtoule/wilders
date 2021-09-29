8481d2ab1267065f2aab9e7048c8b23378ead93d
ChleaBourtoule <chlea.bourtoule@gmail.com>
2021-09-29 11:00:36 +0200

    wilders created with chalk

---
+++ wilders.js
@@ -0,0 +1,4 @@
+const wilders = ['Micka', 'David', 'Patxi', 'Guillaume'];
+const chalk = require('chalk');
+
+console.log(chalk.blue(wilders[0]), chalk.green(wilders[1], chalk.red(wilders[2]), chalk.yellow(wilders[3])));
